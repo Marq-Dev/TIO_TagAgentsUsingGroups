09c2cd28dccc299c5f0482bb80f7727dc1d10f19
Marq-Dev <marq.mercado@gmail.com>
2023-07-21 02:02:52 -0500

    Update TIO_TagAgentsUsingGroups.py

--- TIO_TagAgentsUsingGroups.py
+++ TIO_TagAgentsUsingGroups.py
@@ -10,8 +10,8 @@ def generate_dict(tio):
     tag_dict = {}
     
     for tag in tio.tags.list():
-        if pattern.match(tag["value"]):
-            tag_dict[tag["value"]] = tag["uuid"]
+        if pattern.match(tag['value']):
+            tag_dict[tag['value']] = tag['uuid']
     
     return tag_dict
 
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     tag_dict = generate_dict(tio)
 
     for asset in tio.workbenches.assets(('CATEGORY_PLACEHOLDER', 'CONDITION_PLACEHOLDER', 'TAG_PLACEHOLDER')):
-        if not any(pattern.match(tag["tag_value"]) for tag in asset["tags"]):
-            uuid = asset["tenable_uuid"][0]
+        if not any(pattern.match(tag['tag_value']) for tag in asset['tags']):
+            uuid = asset['tenable_uuid'][0]
             uuid_formatted = '-'.join((uuid[:8],uuid[8:12],uuid[12:16],uuid[16:20],uuid[20:32]))
 
             for agent in tio.agents.list(('uuid', 'eq', uuid_formatted)):
-                group_name = agent["groups"][0]["name"]
-                asset_uuid = asset["id"]
+                group_name = agent['groups'][0]['name']
+                asset_uuid = asset['id']
                 tag_uuid = tag_dict.get(group_name)
                 if tag_uuid:
                     tio.tags.assign(assets=[asset_uuid], tags=[tag_uuid])
