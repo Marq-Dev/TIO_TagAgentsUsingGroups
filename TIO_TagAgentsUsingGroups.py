b377f45ba5cbb2626148b0c8b734afdb6c084bc0
Marq-Dev <marq.mercado@gmail.com>
2023-07-27 09:56:56 -0500

    Update TIO_TagAgentsUsingGroups.py

--- TIO_TagAgentsUsingGroups.py
+++ TIO_TagAgentsUsingGroups.py
@@ -6,41 +6,33 @@ import sys
 import logging
 from tenable.io import TenableIO
 
-# Function for generating a dict of current tags in Tenable.
-# Dict uses the tag name as the key, and the tag UUID as the value.
+# Regex pattern for matching tags.
+pattern = re.compile('REGEX_PLACEHOLDER')
+
+def get_tenable_instance(access_key, secret_key):
+    return TenableIO(access_key, secret_key)
+
+def configure_logging():
+    logging.basicConfig(level=logging.DEBUG)
+
 def generate_dict(tio):
-    
-    tag_dict = {}
-    
+    data_dict = {'tags': {}, 'assets': {}}
+
     try:
         # Queries the "tags" API for any tags that match the regex pattern.
         for tag in tio.tags.list():
-            if pattern.match(tag["value"]):
-                tag_dict[tag["value"]] = tag["uuid"]
-    
-    # Error handling. Exits if an error is encountered.
+            tag_name = tag["value"]
+            tag_uuid = tag["uuid"]
+            if pattern.match(tag_name):
+                data_dict["tags"][tag_name] = tag_uuid
+
     except Exception as e:
-        logging.error(f'An error occurred while fetching tags: {str(e)}')
-        sys.exit()
-    
-    return tag_dict
+        logging.error(f'An error occurred while fetching tags: {e}')
 
-if __name__ == "__main__":
+    return data_dict
 
-    # Configures the API endpoint with an access and secret key.
-    tio = TenableIO(os.environ['TENABLEIO_ACCESS_KEY'], os.environ['TENABLEIO_SECRET_KEY'])
-
-    # Enable logging for debugging purposes.
-    logging.basicConfig(level=logging.DEBUG)
-
-    # Regex pattern for matching tags.
-    pattern = re.compile('REGEX_PLACEHOLDER')
-
-    # Assigns the output of the dict generation function.
-    tag_dict = generate_dict(tio)
-
+def process_assets(tio, tag_dict, asset_dict):
     try:
-        
         # Queries the "assets" API for any assets with tags that match the provided tag in TAG_PLACEHOLDER.
         for asset in tio.workbenches.assets(('CATEGORY_PLACEHOLDER', 'CONDITION_PLACEHOLDER', 'TAG_PLACEHOLDER')):
             # Loops through queried assets checking if any tags match the regex pattern.
@@ -49,30 +41,59 @@ if __name__ == "__main__":
                 uuid = asset["tenable_uuid"][0]
                 # UUID is formatted with hyphens as it's required by the "agents" API.
                 uuid_formatted = '-'.join((uuid[:8],uuid[8:12],uuid[12:16],uuid[16:20],uuid[20:32]))
-
-                try:
-                    # Queries the "agents" API for any agents that match the UUID extracted from the asset.
-                    # Extracts the name of the group and the asset UUID of the agent.
-                    for agent in tio.agents.list(('uuid', 'eq', uuid_formatted)):
-                        group_name = agent["groups"][0]["name"]
-                        asset_uuid = asset["id"]
-                        # Checks if any tag names match the group name in the tag dict.
-                        # Assigns the tag uuid if a match is found.
-                        tag_uuid = tag_dict.get(group_name)
-                        if tag_uuid:
-
-                            try:
-                                # Assigns a tag to the asset that matches the group name.
-                                tio.tags.assign(assets=[asset_uuid], tags=[tag_uuid])
-                            
-                            except Exception as e:
-                                logging.error(f'An error occurred while tagging agents: {str(e)}')
-                                sys.exit()
-
-                except Exception as e:
-                    logging.error(f'An error occurred while processing agents: {str(e)}')
-                    sys.exit()
+                process_agents(tio, tag_dict, asset_dict, asset, uuid_formatted)
 
     except Exception as e:
-        logging.error(f'An error occurred while processing assets: {str(e)}')
-        sys.exit()
+        logging.error(f'An error occurred while processing assets: {e}')
+
+def process_agents(tio, tag_dict, asset_dict, asset, uuid_formatted):
+    try:
+        # Queries the "agents" API for any agents that match the UUID extracted from the asset.
+        # Extracts the name of the group and the asset UUID of the agent.
+        for agent in tio.agents.list(('uuid', 'eq', uuid_formatted)):
+            group_name = agent["groups"][0]["name"]
+            asset_uuid = asset["id"]
+            agent_name = agent["name"]
+            # Checks if any tag names match the group name in the tag dict.
+            # Assigns the tag uuid if a match is found.
+            tag_uuid = tag_dict.get(group_name)
+            if tag_uuid:
+                assign_tag(tio, asset_dict, asset_uuid, tag_uuid, agent_name)
+
+    except Exception as e:
+        logging.error(f'An error occurred while processing agents: {e}')
+
+def assign_tag(tio, asset_dict, asset_uuid, tag_uuid, agent_name):
+    try:
+        if tio.tags.assign(assets=[asset_uuid], tags=[tag_uuid]):
+            asset_dict[agent_name] = asset_uuid
+
+    except Exception as e:
+        logging.error(f'An error occurred while tagging agents: {e}')
+
+def write_to_file(asset_dict):
+    if asset_dict:
+        with open('tagging_results.txt', 'w') as f:
+            for name, uuid in asset_dict.items():
+                f.write(f"(Tagged) Hostname: {name}\n")
+
+def main():
+    # Configures the API endpoint with an access and secret key.
+    tio = get_tenable_instance(os.environ['TENABLEIO_ACCESS_KEY'], os.environ['TENABLEIO_SECRET_KEY'])
+
+    # Enable logging for debugging purposes.
+    configure_logging()
+
+    # Assigns the output of the dict generation function.
+    data_dict = generate_dict(tio)
+
+    tag_dict = data_dict["tags"]
+
+    asset_dict = data_dict["assets"]
+
+    process_assets(tio, tag_dict, asset_dict)
+
+    write_to_file(asset_dict)
+
+if __name__ == "__main__":
+    main()
